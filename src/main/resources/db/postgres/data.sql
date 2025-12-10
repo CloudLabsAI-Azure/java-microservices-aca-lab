36d27bfd117e9bda64dd39a1b2b803018751fb73
amal gireesh <48020356+amalgireesh@users.noreply.github.com>
2025-12-11 02:56:37 +0530

    Replace INSERT IGNORE with INSERT ON CONFLICT

--- src/main/resources/db/postgres/data.sql
+++ src/main/resources/db/postgres/data.sql
@@ -1,4 +1,6 @@
-INSERT IGNORE INTO visits VALUES (1, 7, '2010-03-04', 'rabies shot');
-INSERT IGNORE INTO visits VALUES (2, 8, '2011-03-04', 'rabies shot');
-INSERT IGNORE INTO visits VALUES (3, 8, '2009-06-04', 'neutered');
-INSERT IGNORE INTO visits VALUES (4, 7, '2008-09-04', 'spayed');
+INSERT INTO visits (id, pet_id, visit_date, description) VALUES
+(1, 7, '2010-03-04', 'rabies shot'),
+(2, 8, '2011-03-04', 'rabies shot'),
+(3, 8, '2009-06-04', 'neutered'),
+(4, 7, '2008-09-04', 'spayed')
+ON CONFLICT (id) DO NOTHING;  
